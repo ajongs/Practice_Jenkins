809249f8beb5dc2bd4eba28f83cfb2307d36098b
ajongs <35219960+ajongs@users.noreply.github.com>
2021-07-28 11:46:38 +0900

    first commit

---
+++ src/main/java/com/example/bootex/BootexApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootexApplication.class, args);
+    }
+
+}
